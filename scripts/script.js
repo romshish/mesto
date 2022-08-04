b917ba23d4ef35c88b4f357bdc38985f483f32b1
Roman Shishkin <romshi78@yandex.ru>
2022-08-04 19:17:25 +0300

    fix 1review

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,16 @@
 let editButton = document.querySelector('.profile__button-edit');
 let popupFeater = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_field_name');
+let jobInput = document.querySelector('.popup__input_field_job');
+let nameProfile = document.querySelector('.profile__name');
+let jobProfile = document.querySelector('.profile__job');
 
 function openPopup() {
   popupFeater.classList.add('popup__opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
 
 function closePopup() {
@@ -13,21 +20,13 @@ function closePopup() {
 editButton.addEventListener('click', openPopup);
 closeButton.addEventListener('click', closePopup);
 
-let formElement = document.querySelector('.popup__form');
-
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
-let nameProfile = document.querySelector('.profile__name');
-let jobProfile = document.querySelector('.profile__job');
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
     console.log(nameInput.value);
     console.log(jobInput.value);
+    closePopup()
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-
-
